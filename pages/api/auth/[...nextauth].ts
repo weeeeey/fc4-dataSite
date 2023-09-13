d01c2d9b815b5a989fecf8d53aa0b49f12aaa5e2
zerojin <qser155@naver.com>
2023-09-13 16:32:31 +0900

    credentail / Oauth 설정

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,75 @@
+import NextAuth, { AuthOptions } from 'next-auth';
+
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
+import NaverProvider from 'next-auth/providers/naver';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { client } from '@/lib/prismadb';
+
+import bcrypt from 'bcrypt';
+import axios from 'axios';
+
+export const authOptions: AuthOptions = {
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(client),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID!,
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID!,
+            clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+        }),
+        NaverProvider({
+            clientId: process.env.NAVER_CLIENT_ID!,
+            clientSecret: process.env.NAVER_CLIENT_SECRET!,
+        }),
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('invaild credentials');
+                }
+                const user = await client.user.findUnique({
+                    where: {
+                        email: credentials.email,
+                    },
+                });
+
+                if (!user || !user.hashedPassword) {
+                    throw new Error('inVaild credentials');
+                }
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword
+                );
+                if (!isCorrectPassword) {
+                    throw new Error('Invaild credentials');
+                }
+                return user;
+            },
+        }),
+    ],
+    pages: {
+        signIn: '/',
+    },
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: 'jwt',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
